11ae3badc854b098edcb8a442731d7030e3cdea3
mubking <adeshinamubarak6@gmail.com>
2026-06-28 13:33:18 +0100

    feat: scaffold all core modules

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,15 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { AuthModule } from './auth/auth.module';
+import { AccountsModule } from './accounts/accounts.module';
+import { TransactionsModule } from './transactions/transactions.module';
+import { SorobanModule } from './soroban/soroban.module';
+import { WebhooksModule } from './webhooks/webhooks.module';
+import { SharedModule } from './shared/shared.module';
 
 @Module({
-  imports: [],
+  imports: [AuthModule, AccountsModule, TransactionsModule, SorobanModule, WebhooksModule, SharedModule],
   controllers: [AppController],
   providers: [AppService],
 })
